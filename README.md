db76e6083b640b1b53ce41eec37fe8ba2105eea8
Stephen Mathieson <me@stephenmathieson.com>
2018-08-20 16:18:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# harrisisharrypotter.com
+
+Proof that Harris is actually Harry Potter.
+
+## Development
+
+Do this:
+
+```
+$ yarn
+$ yarn dev
+$ open http://localhost:1234
+```
+
+## Deployment
+
+All commits to the `master` branch are deployed via Netlify.
---
+++ src/app.js
@@ -0,0 +1,26 @@
+const Typed = require('typed.js')
+
+new Typed('.Proof', {
+  strings: [
+    'When Harris dressed as Steve Jobs, everyone could really tell that he was Harry Potter.',
+    'When you <code>/giphy harris &lt;keyword&gt;</code>, a gif of Harry Potter usually shows up.',
+    'Both Harris and Harry have hair.',
+    'Harris is the same age as Harry Potter.',
+    'Harry Potter spelled backwards is Harris.',
+    'Harris is admittedly half wizard.',
+    'Harris dislikes Voldemort a lot.',
+    'Harris has the same middle name as Harry Potter.',
+    'Harris is really good at that game they play where you fly on a broom and hit a ball with magic and stuff.',
+    'Harry Potter is dating a girl named Lucy.',
+    'Harry Potter was born in the same hospital as Harris.',
+    'Harris used to live in a closet in his uncle’s house.',
+    'Harris went to Hogwarts School of Witchcraft and Wizardry for his undergrad.',
+    'Harris’ best friends are named Ron and Hermione.',
+    'Harris spelled backwards is Harry Potter.'
+  ],
+  typeSpeed: 40,
+  shuffle: true,
+  showCursor: false,
+  smartBackspace: true,
+  loop: true
+})
